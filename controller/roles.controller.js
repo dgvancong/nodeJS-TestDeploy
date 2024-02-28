fd790665fcf23692aba8f7fc96a343c8d6e47a5d
Dong Van Cong <dongvancong24@gmail.com>
2024-02-28 10:50:52 +0700

    api roles

--- controller/roles.controller.js
+++ controller/roles.controller.js
@@ -26,6 +26,51 @@ const rolesController = {
                 status: "error"
             })
         }
+    },
+    create: async (req, res) => {
+        try {
+            const { title, content } = req.body
+            const sql = "insert into roles (title, content) values (?, ?)"
+            const [rows, fields] = await pool.query(sql, [title, content])
+            res.json({
+                data: rows
+            })
+        } catch (error) {
+            console.log(error)
+            res.json({
+                status: "error"
+            })
+        }
+    },
+    update: async (req, res) => {
+        try {
+            const { title, content } = req.body
+            const { rolesID } = req.params
+            const sql = "update posts set title = ?, content = ? where rolesID = ?"
+            const [rows, fields] = await pool.query(sql, [title, content, rolesID])
+            res.json({
+                data: rows
+            })
+        } catch (error) {
+            console.log(error)
+            res.json({
+                status: "error"
+            })
+        }
+    }, 
+    delete: async (req, res) => {
+        try {
+            const { rolesID } = req.params
+            const [rows, fields] = await pool.query("delete from roles where rolesID = ?", [rolesID])
+            res.json({
+                data: rows
+            })
+        } catch (error) {
+            console.log(error)
+            res.json({
+                status: "error"
+            })
+        }
     }
 
 }
--- routes/roles-routes.js
+++ routes/roles-routes.js
@@ -1,9 +1,12 @@
 const express = require("express")
-const router = express.Router()
+const rolesRouter = express.Router()
 
 const rolesController = require("../controller/roles.controller")
 
-router.get("/", rolesController.getAll)
-router.get("/:rolesID", rolesController.getById)
+router.get("/", rolesController.getAll),
+router.get("/:id", rolesController.getById),
+router.post("/", rolesController.create),
+router.put("/:id", rolesController.update),
+router.delete("/:id", rolesController.delete),
 
-module.exports = router
+module.exports = rolesRouter
