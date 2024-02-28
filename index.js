56ca9db567597ada9f18ffcf03cd6b9817991e1f
Dong Van Cong <dongvancong24@gmail.com>
2024-02-28 14:32:34 +0700

    Error

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ require('dotenv').config()
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
+// Phần router hệ thống Task Master
 const rolesRouter = require('./routes/roles-routes')
-
 app.use("/api/roles", rolesRouter)
 
-const PORT = process.env.PORT || 5000
 
+
+
+
+//Cổng chạy app
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log("Server is running....")
 })
