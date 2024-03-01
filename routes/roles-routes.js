7387b403fba610d61f365463a52585b4d839cb9b
Dong Van Cong <dongvancong24@gmail.com>
2024-03-01 16:55:45 +0700

    router roles

--- routes/roles-routes.js
+++ routes/roles-routes.js
@@ -5,7 +5,7 @@ const rolesController = require("../controller/roles.controller")
 
 router.get("/", rolesController.getAll)
 router.get("/:id", rolesController.getById)
-router.post("create/", rolesController.create)
+router.post("/create", rolesController.create)
 router.put("/update/:id", rolesController.update)
 router.delete("/delete/:id", rolesController.delete)
 
