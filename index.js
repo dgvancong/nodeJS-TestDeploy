53e9202adaa8fb17f23be0e5c1a48971c2b992cc
Dong Van Cong <dongvancong24@gmail.com>
2024-03-22 14:36:56 +0700

    oce e

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express")
 const app = express()
 
 require('dotenv').config()
-const bcrypt = require('bcrypt');
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
