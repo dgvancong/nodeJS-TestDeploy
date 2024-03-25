1aa8a8f8ffb920a47a7f216263a57f808b91ec10
Dong Van Cong <dongvancong24@gmail.com>
2024-03-25 14:52:41 +0700

    projectteam

--- routes/project-routes.js
+++ routes/project-routes.js
@@ -5,7 +5,7 @@ const projectController = require("../controller/project.controller")
 
 router.get("/", projectController.getAll)
 router.get("/:id", projectController.getById)
-router.get("/projectteam", projectController.projectTeam)
+router.get("/projectteam/", projectController.projectTeam)
 router.post("create/", projectController.create)
 router.put("/update/:id", projectController.update)
 router.delete("/delete/:id", projectController.delete)
